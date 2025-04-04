873ffd2fb582585212e1b13c4a21721a45ecc3a5
Lucas Luann <75109074+LucasLuann@users.noreply.github.com>
2025-04-04 13:02:21 -0400

    fix: pequenos erros que afetaram o build.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true, // Mantém o modo estrito para detectar erros
-  swcMinify: true, // Minifica o SWC para reduzir o tamanho do código
 };
 
 export default nextConfig;
