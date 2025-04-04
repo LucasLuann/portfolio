da02ee84809d586e97b9ac2391d9fcb371a52e74
Lucas Luann <75109074+LucasLuann@users.noreply.github.com>
2025-04-04 11:17:00 -0400

    chore: atualizando dependências

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Mantém o modo estrito para detectar erros
+  swcMinify: true, // Minifica o SWC para reduzir o tamanho do código
 };
 
 export default nextConfig;
